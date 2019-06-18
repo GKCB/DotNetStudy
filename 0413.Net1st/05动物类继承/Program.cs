f883ec15d45ce3eda25deb367e15a79c9b9af6d1
GKCB <hmkscbo@sina.com>
2019-06-18 21:44:29 +0800

    05

---
+++ 0413.Net1st/05动物类继承/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05动物类继承
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Animal
+    {
+        //不确定子类的具体实现,所以应该用抽象函数来实现
+        public abstract void Bark()
+        {
+ 
+        }
+    }
+    class Cat : Animal
+    {
+
+    }
+    class Dog : Animal
+    {
+    }
+    class Pig : Animal
+    { 
+    }
+
+}
