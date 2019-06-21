f98d47cad3cce3d73278949d6c4cc2473a4020e9
GKCB <hmkscbo@sina.com>
2019-06-21 14:45:41 +0800

    11

---
+++ 0413.Net1st/11接口的使用/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _11接口的使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
