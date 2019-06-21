1a167f1c75c2b266cd28f1ebc740c0a8fe5362ac
GKCB <hmkscbo@sina.com>
2019-06-21 10:06:01 +0800

    09

---
+++ 0413.Net1st/09抽象类特点/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _09抽象类特点
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    abstract class Person
+    {
+        public abstract void SayHello();   
+        public abstract void Test();
+    }
+    class student : Person
+    {
+        public override void SayHello()
+        {
+            Console.WriteLine("学生说hello");
+        }
+        public override void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
