692050f88da4ffc2b4a78161bd055fe6e52bde62
GKCB <hmkscbo@sina.com>
2019-06-22 21:26:48 +0800

    12

---
+++ 0413.Net1st/12接口案例/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _12接口案例
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //麻雀 会飞 企鹅不会飞 鸵鸟不会飞 鹦鹉会飞 飞机 会飞
+            IFly[] flys = { new MaBird(), new YingBird(), new Plane() };
+
+            for (int i = 0; i < flys.Length; i++)
+            {
+                flys[i].Fly();
+            }
+        }
+    }
+    class Bird
+    {
+        public void CHLSS()
+        {
+            Console.WriteLine("鸟都会吃喝拉撒睡");
+        }
+    }
+    interface IFly
+    {
+        void Fly();
+    }
+    class MaBird : Bird, IFly
+    {
+
+        public void Fly()
+        {
+            Console.WriteLine("麻雀在飞");
+        }
+    }
+
+    class TuoBird : Bird
+    { }
+
+    class YingBird : Bird, IFly
+    {
+
+        public void Fly()
+        {
+            Console.WriteLine("鹦鹉在飞");
+        }
+    }
+
+    class QQ : Bird
+    { }
+
+    class Plane : IFly
+    {
+        public void Fly()
+        {
+            Console.WriteLine("飞机在飞");
+        }
+    }
+}
