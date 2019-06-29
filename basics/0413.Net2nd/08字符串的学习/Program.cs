d0655c73661893da3749f8946c165aa5d5b09f2d
GKCB <hmkscbo@sina.com>
2019-06-29 09:06:24 +0800

    0413.Net2nd

---
+++ basics/0413.Net2nd/08字符串的学习/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _08字符串的学习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "张三";
+            string s2 = "张三";
+            Console.ReadKey();
+        }
+    }
+}
---
+++ basics/0413.Net2nd/ProOperation/Add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProOperation
+{
+    public class Add:Operation
+    {
+        //父类里面有显示函数，子类也需要实现
+        public Add(int n1, int n2)
+            : base(n1, n2)
+        { }
+        public override int GetResult()
+        {
+            return this.NumberOne + this.NumberTwo;
+        }
+    }
+}
