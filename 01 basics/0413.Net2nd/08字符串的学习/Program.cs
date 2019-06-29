4e84dcf1817e0d068c3394180c4df952eca3e49f
GKCB <hmkscbo@sina.com>
2019-06-29 22:17:13 +0800

    08

--- 01 basics/0413.Net2nd/08字符串的学习/Program.cs
+++ 01 basics/0413.Net2nd/08字符串的学习/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace _08字符串的学习
 {
@@ -11,7 +12,17 @@ namespace _08字符串的学习
         {
             string s1 = "张三";
             string s2 = "张三";
+            Console.WriteLine("s1={0},s1的地址是:{1}", s1, getMemory(s1));
+            Console.WriteLine("s2={0},s2的地址是:{1}", s2, getMemory(s2));
             Console.ReadKey();
         }
+        public static string getMemory(object o) // 获取引用类型的内存地址方法
+        {
+            GCHandle h = GCHandle.Alloc(o, GCHandleType.Pinned);
+            IntPtr addr = h.AddrOfPinnedObject();
+            return "0x" + addr.ToString("X");
+        }
     }
+    
+
 }
