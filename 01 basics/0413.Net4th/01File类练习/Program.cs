7d53b5fedfe45a4a067569518d9cc4f356e9ca98
GKCB <hmkscbo@sina.com>
2019-07-08 11:49:32 +0800

    0413.Net4th.sln

---
+++ 01 basics/0413.Net4th/01File类练习/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace _01File类练习
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //读取"工资.txt"文件，每个人的工资加倍
+            string path = @"../../工资.txt";
+            string[] contents = File.ReadAllLines(path,
+                Encoding.Default);//默认utf-8
+            
+            for (int i = 0; i < contents.Length; i++)
+            {
+                string[] temp = contents[i].Split(new char[]{'\t'},StringSplitOptions.RemoveEmptyEntries);
+                contents[i] = temp[0] + '\t' + int.Parse(temp[1])*2;
+            } 
+            File.WriteAllLines(path,contents,Encoding.Default);
+            Console.WriteLine("操作成功");
+            Console.ReadKey();
+        }
+    }
+}
