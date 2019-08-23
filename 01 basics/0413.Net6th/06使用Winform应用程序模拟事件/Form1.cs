6929161d3f85536fbb3f351b10f285ddf5479e6a
GKCB <hmkscbo@sina.com>
2019-08-23 10:26:43 +0800

    06使用Winform应用程序模拟事件

---
+++ 01 basics/0413.Net6th/06使用Winform应用程序模拟事件/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _06使用Winform应用程序模拟事件
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 01 basics/0413.Net6th/06使用Winform应用程序模拟事件/PlayMusic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace _06使用Winform应用程序模拟事件
+{
+    class PlayMusic
+    {
+        //音乐播放的名字
+        public string Name { get; set; }
+        public PlayMusic(string name)
+        {
+            this.Name = name;
+        }
+        public void PlaySongs()
+        {
+            Console.WriteLine("正在播放"+this.Name);
+            //模拟播放了三秒钟
+            Thread.Sleep(3000);
+            //当播放完成后 执行一个事件
+
+        }
+    }
+}
