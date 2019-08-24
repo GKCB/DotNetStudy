23727616c3b12cb95b928acfd694d586907f5e48
GKCB <hmkscbo@sina.com>
2019-08-24 23:58:56 +0800

    Common

---
+++ 01 basics/0413.Net6th/Common/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Common
+{
+    public class Person
+    {
+        public void Write()
+        {
+            File.WriteAllText("1.txt", "张三李四王五钱六田七");
+        }
+    }
+}
