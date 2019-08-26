d37fca7b87e2f28b019b30edd6d235b9f730ccde
GKCB <hmkscbo@sina.com>
2019-08-26 17:09:13 +0800

    Common

--- 01 basics/0413.Net6th/Common/Person.cs
+++ 01 basics/0413.Net6th/Common/Person.cs
@@ -12,5 +12,19 @@ namespace Common
         {
             File.WriteAllText("1.txt", "张三李四王五钱六田七");
         }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age  = age;
+        }
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public void SayHello()
+        {
+            Console.WriteLine("我是Person类中的函数");
+        }
     }
 }
