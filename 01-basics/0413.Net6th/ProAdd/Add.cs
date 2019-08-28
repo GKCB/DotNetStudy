4d9ccd8a62e8e72e6bd0b40891a11ee7a802392e
GKCB <hmkscbo@sina.com>
2019-08-28 10:14:33 +0800

    ProCalculator

---
+++ 01-basics/0413.Net6th/ProAdd/Add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProOperation;
+
+namespace ProAdd
+{
+    public class Add:Operation
+    {
+        public Add(int n1, int n2)
+            : base(n1,n2)
+        {
+ 
+        }
+        public override string Type
+        {
+            get { return "+"; }
+        }
+
+        public override int GetResult()
+        {
+            return this.NumerOne + this.NumerTwo;
+        }
+    }
+}
---
+++ 01-basics/0413.Net6th/ProFactory/Factory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ProOperation;
+using System.Reflection;
+using System.IO;
+
+namespace ProFactory
+{
+    public class Factory
+    {
+        public static Operation GetOper(string type, int n1, int n2)
+        {
+            Operation oper = null;
+            //动态获得程序集
+            string path = Path.Combine
+                (AppDomain.CurrentDomain.BaseDirectory, "Plug");
+            //文件夹路径
+            //获得PATH路径下所有文件的全路径
+            string[] files = Directory.GetFiles(path,"*.dll");
+            Assembly ass = Assembly.LoadFile(path);
+
+            return oper;
+        }
+    }
+}
